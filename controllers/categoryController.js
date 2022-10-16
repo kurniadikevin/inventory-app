30c2c5195afde9d068fa95c848b644c93974c3a5
kurniadikevin <kurniadikevin@gmail.com>
2022-10-16 19:41:55 +0700

    add CRUD function

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,7 @@
 const Category = require("../models/category");
 const async = require("async");
 const { body, validationResult } = require("express-validator");
+const Product = require("../models/product");
 
 // Display list of all categories.
 exports.category_list = (req, res,next) => {
@@ -40,7 +41,8 @@ exports.category_detail = (req, res,next) => {
         //sucess and render
         res.render('category_detail',{
             title : 'Category Information',
-            category_name : results.category.name
+            category_name : results.category.name,
+            category : results.category
         })
         }
     )
@@ -98,14 +100,73 @@ exports.category_create_post = [
     },
   ];
   
-// Display Category delete form on GET.
-exports.category_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Category delete GET");
+// Display Category delete form on GET
+exports.category_delete_get = (req, res,next) => {
+  async.parallel(
+    {
+      category(callback){
+        Category.findById(req.params.id)
+        .exec(callback)
+      },
+
+    }, (err,results)=>{
+      if(err){
+        return next(err);
+      }
+      if(results.category == null){
+        //no product result
+        res.redirect('/catalog/categories');
+      }
+      //success so render product delete page
+      res.render('category_delete',{
+        title: "Delete Category",
+        categoryName : results.category.name,
+        category : results.category
+      })
+    }
+  )
 };
 
-// Handle Category delete on POST.
+// Handle category delete on POST.
 exports.category_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Category delete POST");
+  async.parallel(
+    {
+      category(callback){
+        Category.findById(req.body.categoryid).exec(callback);
+
+      },
+      category_product(callback){
+        Product.find({ category : req.params.id}).exec(callback);
+      }
+    },
+    (err,results) => {
+      if(err) {
+        return next(err);
+      }
+      // there is still product that assign to the category
+      if(results.category_product.length > 0){
+        res.render('category_delete',{
+          title: "Delete Category",
+          info : "Cannot delete category. There is still product that assign to this category",
+          categoryName : results.category.name,
+          category : results.category
+        });
+        return;
+      }
+
+      //success
+      Category.findByIdAndRemove(req.body.categoryid,
+        (err)=> {
+          //if error happen when removing
+          if(err){
+            return next(err);
+          }
+          // Sucessfully remove then redirect
+          res.redirect('/catalog/categories')
+        })
+
+    }
+  )
 };
 
 // Display Category update form on GET.
--- controllers/productController.js
+++ controllers/productController.js
@@ -16,7 +16,7 @@ exports.index = (req, res) => {
     },
     (err, results) => {
       res.render("index", {
-        title: "Local Library Home",
+        title: "Record Library Home",
         error: err,
         data: results,
       });
@@ -65,7 +65,8 @@ exports.product_detail = (req, res, next) => {
         product: results.product,
         numberOfStock : results.product.numberOfStock,
         price : results.product.price,
-        category : results.product.category
+        category : results.product.category,
+        description : results.product.description
       });
     }
   );
@@ -154,21 +155,154 @@ if (!errors.isEmpty()) {
 
 
 // Display product delete form on GET.
-exports.product_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Product delete GET");
+exports.product_delete_get = (req, res,next) => {
+  async.parallel(
+    {
+      product(callback){
+        Product.findById(req.params.id)
+        .populate('category')
+        .exec(callback)
+      }
+    }, (err,results)=>{
+      if(err){
+        return next(err);
+      }
+      if(results.product == null){
+        //no product result
+        res.redirect('/catalog/products');
+      }
+      //success so render product delete page
+      res.render('product_delete',{
+        title: "Delete Product",
+        productName : results.product.name,
+        product : results.product,
+        category : results.product.category
+      })
+    }
+  )
 };
+
 
 // Handle product delete on POST.
 exports.product_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Product delete POST");
+  async.parallel(
+    {
+      product(callback){
+        Product.findById(req.body.productid).exec(callback);
+
+      }
+    },
+    (err,results) => {
+      if(err) {
+        return next(err);
+      }
+      //success
+      Product.findByIdAndRemove(req.body.productid,
+        (err)=> {
+          //if error happen when removing
+          if(err){
+            return next(err);
+          }
+          // Sucessfully remove then redirect
+          res.redirect('/catalog/products')
+        })
+
+    }
+  )
 };
 
 // Display product update form on GET.
-exports.product_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Product update GET");
+exports.product_update_get = (req, res,next) => {
+  async.parallel(
+    {
+      product(callback){
+        Product.findById(req.params.id)
+        .populate('category').exec(callback);
+      },
+      category(callback){
+        Category.find({}, "name").exec(callback)
+      }
+    },
+    (err,results) => {
+      if(err){
+        return next(err);
+      }
+      if(results.product == null){
+        // no results
+        const err = new Error("Product not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Success.
+      res.render('product_form',{
+        title : 'Update Product',
+        product : results.product,
+        category : results.category
+      })
+    }
+  )
 };
 
 // Handle product update on POST.
-exports.product_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Product update POST");
-};
+exports.product_update_post = [
+  //validate and sanitize form input
+  body("name", "Product name required").trim().isLength({ min: 1 }).escape(),
+  body("description", "Product description required").trim().isLength({ min: 1 }).escape(),
+  body("category", "Category name required").escape(),
+  body("price", "Price required").trim().isLength({ min: 1 }).escape(),
+  body("numberOfStock", "Number of stock required").trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Book object with escaped and trimmed data.
+    const product = new Product({
+      name: req.body.name,
+      description : req.body.description,
+      category : req.body.category,
+      price : req.body.price,
+      numberOfStock : req.body.numberOfStock,
+      _id : req.params.id,
+    });
+////////////////////////////////////////
+if (!errors.isEmpty()) {
+  // There are errors. Render form again with sanitized values/error messages.
+
+  // Get all category for form
+  async.parallel(
+    {
+      categories(callback){
+        Category.find(callback)
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+
+       // Mark our selected genres as checked.
+       for (const category of results.categories) {
+        if (product.category.includes(category._id)) {
+          category.checked = "true";
+        }
+      }
+
+      res.render('product_form',
+        {title : 'Create New Product',
+        category : results.categories});
+    });
+    return;
+  }
+   // Data from form is valid. Update the product.
+   Product.findByIdAndUpdate(req.params.id, product, {}, (err, product) => {
+    if (err) {
+      return next(err);
+    }
+    // Successful: redirect to new product record.
+    res.redirect(product.url);
+  });
+  }
+];
+ 
