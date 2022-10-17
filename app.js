7aec4e9dde81cd00997bb92295ab7bde4fe899d8
kurniadikevin <kurniadikevin@gmail.com>
2022-10-17 13:41:05 +0700

    pre deploy commit

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter =  require('./routes/catalog');
+const compression = require("compression");
+const helmet = require("helmet");
+
 
 var app = express();
-
+app.use(helmet());
 
 //set up mongodb connection with mongoose
 const mongoose = require("mongoose");
@@ -23,6 +26,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(compression())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
